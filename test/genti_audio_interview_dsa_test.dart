39ed561a73ce5c783b18ec48f1c2065490a5e62f
Bishop <bishopuzochukwu37@gmail.com>
2022-10-06 00:01:06 +0100

    added tests

--- test/genti_audio_interview_dsa_test.dart
+++ test/genti_audio_interview_dsa_test.dart
@@ -2,7 +2,11 @@ import 'package:genti_audio_interview_dsa/genti_audio_interview_dsa.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
+  test('Fibonacci of 12', () {
+    expect(fibonacci(12), 144);
+  });
+
+  test('Missing number in array', () {
+    expect(findMissing([1, 2, 4, 6, 3, 7, 8]), 5);
   });
 }
